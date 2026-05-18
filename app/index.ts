19549a260dd436de3f6d6b41f6d94a2a5c435aef
AngelStarfruit <angelhernandezcastillo38@gmail.com>
2026-05-18 13:57:35 -0600

    terciarios

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './ListaDePrecios';
+import App from './Dashboard';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
